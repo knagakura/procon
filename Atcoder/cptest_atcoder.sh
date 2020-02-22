2890c789bf3af0190b3f7a22a4e1c0b4aec8f16f
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-02-23 02:09:06 +0900

    拡張子ごとに処理を変えた

--- Atcoder/cptest_atcoder.sh
+++ Atcoder/cptest_atcoder.sh
@@ -5,10 +5,23 @@ problemname=$2
 filename=${3:-${problemname}.cpp}
 taskname=${4:-${contestname}}
 oj dl "https://atcoder.jp/contests/${contestname}/tasks/${taskname}_${problemname}"
-echo g ./${filename}
+fext="${filename##*.}"
 
-g++-9 -O2 -std=gnu++14 -Wall -Wextra -Wshadow ./${filename}
+if [ ${fext} = "cpp" ] ; then
+    echo g ./${filename}
+    g++-9 -O2 -std=gnu++14 -Wall -Wextra -Wshadow ./${filename}
+    oj t
+elif [ ${fext} = "py" ] ; then
+    echo python3 ${filename}
+    oj t -c "python3 ${filename}"
+elif [ ${fext} = "jl" ] ; then
+    echo julia ${filename}
+    oj t -c "julia ${filename}"
+else
+    echo "拡張子が正しくありません"
+fi
 
-oj t
+
+
 rm -f a.out
 rm -rf test

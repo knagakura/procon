2b133e60544c4bac2a5b2a06fd270c4511e37065
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-04-29 22:10:20 +0900

    update

--- atcoder/submit_atcoder.sh
+++ atcoder/submit_atcoder.sh
@@ -8,20 +8,21 @@ taskname=${4:-${contestname}}
 fext="${filename##*.}"
 
 
-language=${3003}
-if [ ${fext} = "cpp" ] ; then
-    language=3003
-elif [ ${fext} = "jl" ] ; then
-    language=3018
-elif [ ${fext} = "py" ] ; then
-    echo -n which language, py, pypy?:\ ; read
-    if [ $REPLY = "pypy" ] ; then
-        language=3510
-    elif [ $REPLY = "py" ] ; then
-        language=3023
-    else
-        echo 入力が間違っています
-        exit
-    fi
-fi
-oj s -l ${language} "https://atcoder.jp/contests/${contestname}/tasks/${taskname}_${problemname}" ${filename}
+#language=${3003}
+#if [ ${fext} = "cpp" ] ; then
+#    language=3003
+#elif [ ${fext} = "jl" ] ; then
+#    language=3018
+#elif [ ${fext} = "py" ] ; then
+#    echo -n which language, py, pypy?:\ ; read
+#    if [ $REPLY = "pypy" ] ; then
+#        language=3510
+#    elif [ $REPLY = "py" ] ; then
+#        language=3023
+#    else
+#        echo 入力が間違っています
+##        exit
+#  fi
+#fi
+oj s "https://atcoder.jp/contests/${contestname}/tasks/${taskname}_${problemname}" ${filename}
+#oj s -l ${language} "https://atcoder.jp/contests/${contestname}/tasks/${taskname}_${problemname}" ${filename}

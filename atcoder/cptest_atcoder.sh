4baabe433e58b6092c823fc071ed115992b20232
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-08-06 18:57:46 +0900

    oj dを最初だけ行うように設定

--- atcoder/cptest_atcoder.sh
+++ atcoder/cptest_atcoder.sh
@@ -6,24 +6,28 @@ problemname=$1
 filename=${2:-${problemname}.cpp}
 contestname=${3:-${working_folder}}
 taskname=${4:-${contestname}}
-oj dl "https://atcoder.jp/contests/${contestname}/tasks/${taskname}_${problemname}"
+
+
+sample_dir=sample_${problemname}
+if [[ ! -e ${sample_dir} ]]; then
+    oj dl -d ${sample_dir} "https://atcoder.jp/contests/${contestname}/tasks/${taskname}_${problemname}"
+fi
+
 fext="${filename##*.}"
 
 if [ ${fext} = "cpp" ] ; then
     echo g ./${filename}
     g++-9 -O2 -std=gnu++17 -Wall -Wextra ./${filename}
-    oj t
+    oj t -d ${sample_dir} -i
 elif [ ${fext} = "py" ] ; then
     echo python3 ${filename}
-    oj t -c "python3 ${filename}"
+    oj t -d ${sample_dir} -c "python3 ${filename}" -i
 elif [ ${fext} = "jl" ] ; then
     echo julia ${filename}
-    oj t -c "julia ${filename}"
+    oj t -d ${sample_dir} -c "julia ${filename}" -i
 else
     echo "拡張子が正しくありません"
 fi
 
-
-
 rm -f a.out
-rm -rf test
+# rm -rf ${sample_dir}
--- codeforces/cft.sh
+++ codeforces/cft.sh
@@ -5,19 +5,20 @@ working_folder=${PWD##*/}
 problemname="$1"
 filename=${2:-${problemname}.cpp}
 contestname=${3:-${working_folder}}
-oj dl https://codeforces.com/contest/${contestname}/problem/${problemname}
+
+sample_dir=sample_${problemname}
+if [[ ! -e ${sample_dir} ]]; then
+    oj dl -d ${sample_dir} https://codeforces.com/contest/${contestname}/problem/${problemname}
+fi
 
 fext="${filename##*.}"
 
 if [ ${fext} = "cpp" ] ; then
     echo g ./${filename}
-    g++-9 -O2 -std=gnu++17 -Wall -Wextra ./${filename}
-    oj t
+    g++-9 -O2 -std=gnu++17 -Wall -Wextra -DDEBUG ./${filename}
+    oj t -d ${sample_dir} -i
 else
     echo "拡張子が正しくありません"
 fi
 
-
-
 rm -f a.out
-rm -rf test

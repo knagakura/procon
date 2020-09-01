88741c8dd32e6c3f92c2d18fa7aed40e6e5db20e
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-09-02 00:21:05 +0900

    a

---
+++ atcoder/dp/f2.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,N) for(int i=0;i<int(N);++i)
+#define rep1(i,N) for(int i=1;i<int(N);++i)
+#define all(a) (a).begin(),(a).end()
+#define bit(k) (1LL<<(k))
+#define SUM(v) accumulate(all(v), 0LL)
+
+typedef pair<int, int> i_i;
+typedef pair<ll, ll> l_l;
+template <class T> using vec = vector<T>;
+template <class T> using vvec = vector<vec<T>>;
+struct fast_ios{ fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }fast_ios_;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define TOSTRING(x) string(#x)
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (T &x : vec) is >> x; return is; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { os  << "["; for(auto _: v) os << _ << ", "; os << "]" << endl; return os; };
+template <typename T> ostream &operator<<(ostream &os, set<T> &st) { os << "("; for(auto _: st) { os << _ << ", "; } os << ")";return os;}
+template <typename T, typename U> ostream &operator<<(ostream &os, const pair< T, U >& p){os << "{" <<p.first << ", " << p.second << "}";return os; }
+template <typename T, typename U> ostream &operator<<(ostream &os, const map<T, U> &mp){ os << "["; for(auto _: mp){ os << _ << ", "; } os << "]" << endl; return os; }
+
+#define DUMPOUT cerr
+void dump_func(){ DUMPOUT << endl; }
+template <class Head, class... Tail> void dump_func(Head &&head, Tail &&... tail) { DUMPOUT << head; if (sizeof...(Tail) > 0) { DUMPOUT << ", "; } dump_func(std::move(tail)...); }
+
+#ifdef DEBUG
+#define dbg(...) dump_func(__VA_ARGS__)
+#define dump(...) DUMPOUT << string(#__VA_ARGS__) << ": "; dump_func(__VA_ARGS__)
+#else
+#define dbg(...)
+#define dump(...)
+#endif
+
+const int INF = (ll)1e9;
+const ll INFLL = (ll)1e18+1;
+const ll MOD = 1000000007;
+// const ll MOD = 998244353;
+const long double PI = acos(-1.0);
+
+/*
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+const string dir = "DRUL";
+*/
+
+ll dp[3030][3030];
+int main() {
+    string S, T;
+    cin >> S >> T;
+    int N = S.size();
+    int M = T.size();
+    rep(i,N+1)rep(j,M+1)dp[i][j] = 0;
+    rep(i,N)rep(j,M){
+        if(S[i] == T[j])chmax(dp[i+1][j+1], dp[i][j] + 1);
+        chmax(dp[i+1][j+1], dp[i+1][j]);
+        chmax(dp[i+1][j+1], dp[i][j+1]);
+    }
+    #ifdef DEBUG
+    rep(i,N+1){
+        rep(j,M+1){
+            cerr << dp[i][j] << " ";
+        }
+        cerr << endl;
+    }
+    #endif
+    string ans;
+    int X = N, Y = M;
+    while(X > 0 && Y > 0){
+        if(dp[X][Y] == dp[X][Y-1]){
+            Y--;
+        }
+        else if(dp[X][Y] == dp[X-1][Y]){
+            X--;
+        }
+        else{
+            ans.push_back(S[X-1]);
+            X--, Y--;
+        }
+    }
+    reverse(all(ans));
+    cout << ans << endl;
+}

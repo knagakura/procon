3fe0f3051cf7593c90f9021bafeca720b60067f9
knagakura <knagakura@bs.s.u-tokyo.ac.jp>
2020-11-06 04:23:26 +0900

    これは何

--- others/hallab/hpc2020/Combine.d
+++ others/hallab/hpc2020/Combine.d
@@ -6,4 +6,4 @@ Combine.o: Combine.cpp src/Game.cpp src/Game.hpp src/Answer.hpp \
  src/Simulator.hpp src/Math.cpp src/Parameter.cpp src/Rabbit.cpp \
  src/Random.cpp src/RandomSeed.cpp src/Recorder.cpp src/Scroll.cpp \
  src/Simulator.cpp src/Stage.cpp src/Timer.cpp src/Vector2.cpp \
- src/Answer.cpp
+ src/Answer2.cpp
